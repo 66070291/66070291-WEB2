5c430d031e3d047926c9f3147282a5068ce9928d
66070291 <66070291@kmitl.ac.th>
2023-10-27 12:51:57 +0700

    aa

--- index.js
+++
@@ -1,6 +0,0 @@
-function setusername(){
-    document.getElementById("username-p").innerHTML = document.getElementById("username").value + "'s Gallery";
-}
-function setprofile(){
-    document.getElementById("ProfileImg").style.backgroundImage = "url(" + document.getElementById("profileimg").value + ")";
-}
